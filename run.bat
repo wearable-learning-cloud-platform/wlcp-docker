e203436833ab725913d69981cea169cce170fede
mmicciolo <mmicciolo@wpi.edu>
2020-04-27 13:36:17 -0400

    Updated start scripts

--- run.bat
+++ run.bat
@@ -1,2 +1,3 @@
 aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 397120343186.dkr.ecr.us-east-2.amazonaws.com
+docker-compose pull
 docker-compose up
---
+++ run.sh
@@ -0,0 +1,1 @@
+#!/bin/bashaws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 397120343186.dkr.ecr.us-east-2.amazonaws.comdocker-compose pulldocker-compose up
